2c2ec11f74f54fc90ed9176fff9e205c87d2ba79
俞书昊 <yushuhao@xiaohongshu.com>
2026-02-16 10:26:11 +0800

    移除监控

--- src/main/java/org/buaa/project/ProjectApplication.java
+++ src/main/java/org/buaa/project/ProjectApplication.java
@@ -1,12 +1,8 @@
 package org.buaa.project;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -18,12 +14,4 @@ public class ProjectApplication {
         SpringApplication.run(ProjectApplication.class, args);
     }
 
-    /**
-     * 配置普罗米斯修中显示的服务名称
-     */
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
-        System.out.println("applicationName = " + applicationName);
-        return (registry -> registry.config().commonTags("application", applicationName));
-    }
 }
